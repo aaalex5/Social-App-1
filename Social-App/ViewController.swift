0bbc01cb887c492b74c04a5c8a63ba3ba52d2ff3
Alex Alajbegovic <aaalex@alexs-mbp-3.attlocal.net>
2018-05-14 16:55:45 -0400

    Initial Commit

---
+++ Social-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Social-App
+//
+//  Created by Alex Alajbegovic on 5/14/18.
+//  Copyright © 2018 Alex Alajbegovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

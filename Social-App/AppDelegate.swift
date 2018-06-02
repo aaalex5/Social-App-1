8070f9954347ce4aaf5e50956f68cc463aaa9b12
Alex Alajbegovic <aaalex@alexs-macbook-pro-3.local>
2018-06-02 16:41:24 -0400

    Editted and commented on Samy's great work!

--- Social-App/AppDelegate.swift
+++ Social-App/AppDelegate.swift
@@ -18,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate  {
     //Testing push source control...
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure() // Override point for customization after application launch.
+        
+        //Configures firebase to app
+        FirebaseApp.configure()
         
         return true
     }
--- Social-App/ViewController.swift
+++ Social-App/ViewController.swift
@@ -11,73 +11,75 @@ import FirebaseAuth
 
 
 class ViewController: UIViewController {
-
+    
+    //Outlets for sign in page
     @IBOutlet weak var selector: UISegmentedControl!
-    
     @IBOutlet weak var signinLabel: UILabel!
-   
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
     @IBOutlet weak var signinButton: UIButton!
     
+    //Sign-in selector helper var
     var isSignIn:Bool = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //Sign in selector is an ACTION (it does something when used)
     @IBAction func selectorChanged(_ sender: UISegmentedControl) {
+        //Flips the boolean of selector
         isSignIn = !isSignIn
         
+        //Changes text of "signinLabel" depending on the selector bool
         if isSignIn {
             signinLabel.text = "Sign In"
             signinButton.setTitle("Sign in", for: .normal)
-        }
-        
-        else{
+        } else {
             signinLabel.text = "Register"
             signinButton.setTitle("Register", for: .normal)
         }
-    }
+    } //selectorChanged
     
+    //Action for the sign in BUTTON
     @IBAction func signinButtonTapped(_ sender: UIButton) {
-        if let email = emailTextField.text, let pass = passwordTextField.text{
-            if isSignIn{
+        //Create two const var for email and password grabbed from
+        //the text fields for those
+        if let email = emailTextField.text, let pass = passwordTextField.text {
+            
+            if isSignIn {
+                //Firebase authentication command (ACCESS EXISTING USER)
                 Auth.auth().signIn(withEmail: email, password: pass) { (user, error) in
                     if let u = user {
-                        // go to next page
+                        // go to next page (user is found)
                         self.performSegue(withIdentifier: "GoToHome", sender: self)
-                    }
-                    else {
+                    } else {
                         // Check error and show message
-                    }
+                        }
                     }
                 
-            }
-            else {
+            } else {
+                //Firebase Auth command (CREATES a new user)
                 Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
                     if let u = user {
                         self.performSegue(withIdentifier: "GoToHome", sender: self)
-                    }
-                    else {
+                    } else {
                         // Check error and show message
+                        }
                     }
                 }
-                }
-            }
-           
-                
-            }
-        }
+            } //if email and pass have a value
+        
+            } //signinButtonTapped
+    
+        }//ViewController
     
 
 

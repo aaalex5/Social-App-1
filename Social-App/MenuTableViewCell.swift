e19651c43a597e2c25966310f6473bc1692d83f0
Alex Alajbegovic <aaalex@alexs-macbook-pro-3.local>
2018-06-07 14:53:33 -0400

    Finished the Side menu tutorial (NOT sure if we're gonna use this, but it's a good start)

---
+++ Social-App/MenuTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuTableViewCell.swift
+//  Social-App
+//
+//  Created by Alex Alajbegovic on 6/7/18.
+//  Copyright © 2018 Alex Alajbegovic. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Social-App/MenuViewController.swift
+++ Social-App/MenuViewController.swift
@@ -12,8 +12,10 @@ protocol SlideMenuDelegate {
     func slideMenuSelectedAt(_ index : Int32)
 }
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var buttonMenuOverlay: UIButton!
     var buttonMenu : UIButton!
     var delegate : SlideMenuDelegate?
     
@@ -30,7 +32,50 @@ class MenuViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    //Allows for the menu to retract when the main screen is touched instead of
+    //the button
+    @IBAction func buttonCloseTapped(_ sender: UIButton) {
+        buttonMenu.tag = 0
+        buttonMenu.isHidden = false
+        if (self.delegate != nil) {
+            var index = Int32(sender.tag)
+            if (sender == self.buttonMenuOverlay) {
+                index = -1
+            }
+            delegate?.slideMenuSelectedAt(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 0, width:
+                UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = UIColor.clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        })
+    }
+    
+    @IBAction func menuButtonTapped(_ sender: Any) {
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SideMenuCell") as! MenuTableViewCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let mainStoreboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let DVC = mainStoreboard.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+        self.navigationController?.pushViewController(DVC, animated: true)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
